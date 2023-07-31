753cac7286a08f50105f53475dbe746cd930bf42
devartes <anacavalcante731@gmail.com>
2023-07-31 07:28:08 -0300

    refactor: add hover effect to Community and AboutUs links and fix some styling

--- components/ui/Home/Community.tsx
+++ components/ui/Home/Community.tsx
@@ -33,7 +33,7 @@ export default function Community(props: Props) {
       class="h-full w-full relative lg:py-[130px] md:py-[80px] py-[60px]"
     >
       <div class="xl:max-w-[1320px] lg:max-w-[960px] md:max-w-[720px] sm:max-w-[540px] w-full mx-auto h-full md:pb-[28px] md:mb-[-20px] pb-[15px] px-3">
-        <div class="rounded-[15px] py-[56px] px-[30px] bg-[#5034fcd9] lg:w-[46%] md:w-[66.66667%] sm:w-[83.33333%] w-full flex flex-col items-center mx-auto">
+        <div class="rounded-[15px] py-[56px] px-[30px] bg-[#5034fcd9] lg:w-[50%] md:w-[66.66667%] sm:w-[83.33333%] w-full flex flex-col items-center mx-auto">
           <span
             ref={spanRef}
             class="bg-[#fcdc00] text-[#262729] min-w-[112px] h-[30px] mb-[22px] text-[13px] font-semibold leading-[22px] rounded-[15px] flex items-center justify-center leadin-[1.875]"
@@ -55,7 +55,7 @@ export default function Community(props: Props) {
           <div ref={linkRef}>
             <a
               href={props.linkUrl}
-              class="text-white bg-[#e71460] rounded-[50px] p-[5px] leading-[28px] font-semibold w-fit min-w-[183px] h-[56px] text-[15px] inline-flex items-center justify-center shadow--primary-2"
+              class="text-white bg-[#e71460] rounded-[55px] p-[5px] leading-[28px] font-semibold w-fit min-w-[163px] h-[56px] text-[15px] inline-flex items-center justify-center shadow--primary-2 hover:bg-[#ff7043] hover:border hover:border-[#ff6838]"
               target="_blank"
             >
               {props.link}
